47f9ead71c5e807a220324a2933300ee79511c73
GALANG08 <99567510+GALANG08@users.noreply.github.com>
2022-02-16 19:13:15 +0700

    Add files via upload

---
+++ HALAL.sh
@@ -0,0 +1,151 @@
+#!/bin/sh
+
+# code warna
+
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+
+# hapus texs
+clear
+# tampipan
+figlet Surah
+echo $purple"================================================"
+echo "[+] Author : Galang"
+echo "[+] Team   : Haxor Jawa"
+echo "[+] Yt     : Haxor Jawa"
+echo "================================================"
+echo
+echo $cyan"# STAY HALAL BROTHER"
+echo "================================================"
+echo
+sleep 1
+     echo "0). Ikut Grup"
+     echo "1). Surah al-fatihah"
+     echo "2). Surah Annas"
+     echo "3). Tutorial Good Loking Ala Islam"
+     echo "4). Cara Meluluhkan Hati Orang Yg kita Suka"
+     echo "5). Juz-amma Lengkap"
+     echo "6). Keluar"
+echo $blue"================================================"
+echo
+sleep 1
+read -p "pilih: " Surah
+
+if [ $Surah = 0 ]
+then
+    xdg-open https://chat.whatsapp.com/KHxqAIZ7tyj0EyAq4UiimQ
+fi
+
+if  [ $Surah = 1 ]
+then
+
+  echo $yellow"Surah Alfatihah"
+  echo "==================="
+  echo
+  echo "Bismillaahir Rahmaanir Rahiim"
+  echo
+  echo " Dengan nama Allah Yang Maha Pengasih, Maha Penyayang"
+  echo
+  echo "2).Alhamdu lillaahi Rabbil 'aalamiin"
+  echo
+  echo "Segala puji bagi Allah, Tuhan seluruh alam,"
+  echo
+  echo "3).Ar-Rahmaanir-Rahiim"
+  echo
+  echo "Yang Maha Pengasih, Maha Penyayang,"
+  echo
+  echo "4).Maaliki Yawmid-Diin"
+  echo
+  echo "Pemilik hari pembalasan"
+  echo
+  echo "5).Iyyaaka na'budu wa lyyaaka nasta'iin"
+  echo
+  echo "Hanya kepada Engkaulah kami menyembah dan hanya kepada Engkaulah kami mohon pertolongan."
+  echo
+  echo "6).Ihdinas-Siraatal-Mustaqiim"
+  echo
+  echo "Tunjukilah kami jalan yang lurus"
+  echo
+  echo "7).Siraatal-laziina an'amta 'alaihim ghayril-maghduubi 'alaihim wa lad-daaalliin"
+  echo
+  echo "(yaitu) jalan orang-orang yang telah Engkau beri nikmat kepadanya; bukan (jalan) mereka yang dimurkai, dan bukan (pula jalan) mereka yang sesat."
+  echo
+fi
+
+if  [ $Surah = 2 ]
+then
+
+echo $yellow"Surah annas"
+echo "==================="
+echo
+echo "Bismillaahir Rahmaanir Rahii"
+echo
+echo "Dengan nama Allah Yang Maha Pengasih, Maha Penyayang"
+echo
+echo "1).Qul a'uzu birabbin naas"
+echo
+echo "Katakanlah,Aku berlindung kepada Tuhannya manusia"
+echo
+echo "2).Malikin naas"
+echo
+echo "Raja manusia"                                                                                                   echo
+echo "3).Ilaahin naas"
+echo
+echo "sembahan manusia"
+echo
+echo "4).Min sharril was waasil khannaas"
+echo
+echo "dari kejahatan bisikan setan yang bersembunyi,"
+echo
+echo "5).Al lazii yuwas wisu fii suduurin naas"
+echo
+echo "yang membisikkan kejahatan ke dalam dada manusia"
+echo
+echo "6).Minal jinnati wan naas"
+echo
+echo "dari golongan jin dan manusia."
+echo
+
+fi
+if [ $Surah = 3 ]
+then
+
+echo $yellow"Cara good loking"
+echo "============================"
+echo "1. Menjaga Kebersihan"
+echo
+echo "2. Memakai Pakaian Yang Layak"
+echo
+echo "3. Makan Dan Minum Secukupnya"
+echo
+echo "4. Rutin Berolahraga"
+echo
+
+fi
+
+if [ $Surah = 4 ]
+then
+
+echo $yellow "Cara Meluluhkan Hati Orang Yg Kita Suka"
+echo "==============================================="
+echo
+echo "Allahumma Inni-As'aluka Hubbaka Wa Hubba Man Yuhibbuka Wal-Amalalladhi Yuballighuni Hubbak."
+echo
+
+fi                                                                                                                  
+
+if [ $Surah = 5 ]
+then
+
+echo $yellow "Tempel ini di Google"
+echo "========================"
+echo
+echo "https://kalam.sindonews.com/quran"
+echo
+
+fi
